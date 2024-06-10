22a7d6e4567d1652175ca8408c282851f918a9da
Yasen Simeonov <y.simeonov@cnsys.bg>
2024-06-10 16:59:53 +0300

    Initial commit

---
+++ MS_WinServiceApp_NetStandard/MyNewService.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace MS_WinServiceApp_NetStandard
+{
+    partial class MyNewService
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            // 
+            // MyNewService
+            // 
+            this.ServiceName = "MyNewService";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+
+        }
+
+        #endregion
+
+        public System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ MS_WinServiceApp_NetStandard/MyNewService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace MS_WinServiceApp_NetStandard
+{
+    public partial class MyNewService : ServiceBase
+    {
+        private int eventId = 1;
+
+        public MyNewService()
+        {
+            InitializeComponent();
+            eventLog1 = new EventLog();
+            if (!EventLog.SourceExists("MySource"))
+            {
+                EventLog.CreateEventSource("MySource", "MyNewLog");
+            }
+            eventLog1.Source = "MySource";
+            eventLog1.Log = "MyNewLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("In OnStart.");
+
+            // Set up a timer that triggers every minute.
+            Timer timer = new Timer
+            {
+                Interval = 60000 // 60 seconds
+            };
+            timer.Elapsed += new ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+
+        public void OnTimer(object sender, ElapsedEventArgs args)
+        {
+            // TODO: Insert monitoring activities here.
+            eventLog1.WriteEntry("Monitoring the System", EventLogEntryType.Information, eventId++);
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("In OnStop.");
+        }
+    }
+}
